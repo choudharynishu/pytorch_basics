44490802c8afca126bc1fb051155e3cdf410c364
Nishu Choudhary <nishuchoudhary@gatech.edu>
2024-05-01 15:47:24 -0500

    Added Neural Network

--- optimization.py
+++ optimization.py
@@ -89,15 +89,16 @@ can be done using an object created using transforms.ToTensor().
  
 From documentation, 
 transforms.ToTensor(): converts a PIL Image or ndarray (H*W*C) in the range of [0, 255] to a torch.FloatTensor 
-(C * H * W) in range of [0.0, 1.0].
+(C * H * W).
 """
 # ----- Data Transformation Pipeline
-transform_pipeline = transforms.Compose([transforms.ToTensor()])
+transform_pipeline = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize((0.2861,), (0.3530,))])
 # ----- Load the Dataset
 train_dataset = FashionMNIST(root=dataset_path, train=True, transform=transform_pipeline, download=True)
 test_dataset = FashionMNIST(root=dataset_path, train=False, transform=transform_pipeline, download=True)
 # ----- Check the range of a random tensor from the training_dataset
-
+print(f"Mean of the tensors in the Training is: {(train_dataset.data.float()).mean().item()}")
 # ----- Divide the dataset into Training and Validation set
 train_size = len(train_dataset)
 train_length, val_length = int(0.8 * train_size), int(0.2 * train_size) # Divide data by 80-20 rule
@@ -107,6 +108,19 @@ train_loader = data.DataLoader(train_set, batch_size=1024, shuffle=True)
 validation_loader = data.DataLoader(validation_set, batch_size=1024, shuffle=True)
 test_loader = data.DataLoader(test_dataset, batch_size=1024, shuffle=True)
 # ---------------------------------------------------Neural Network Architecture-------------------------------------- #
+class BaseNetwork(nn.Module):
+    def __init__(self, activation_func=nn.Sigmoid(), input_size=784, output_classes=10, hidden_layers=[512, 256, 256, 128]):
+        super().__init__()
+        layers = []
+        input_dims = [input_size]+hidden_layers
+        output_dims = hidden_layers + [output_classes]
+        for (input_dim, output_dim) in zip(input_dims, output_dims):
+            layers += [nn.Linear(input_dim, output_dim), activation_func]
+        self.layers = nn.Sequential(*layers)
+        print(layers)
+
+model = BaseNetwork()
+
 # ---------------------------------------------------Visualizations--------------------------------------------------- #
 # ---------------------------------------------------Initialization Techniques---------------------------------------- #
 # ------- Constant Initialization
