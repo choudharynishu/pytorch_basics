05b54f8dbdecb0f289e55f1ca2559dbbcd2c2a8b
Nishu Choudhary <nishuchoudhary@gatech.edu>
2024-06-10 20:32:54 -0500

    revisit optimization script

--- optimization.py
+++ optimization.py
@@ -97,16 +97,20 @@ transform_pipeline = transforms.Compose([transforms.ToTensor(),
 # ----- Load the Dataset
 train_dataset = FashionMNIST(root=dataset_path, train=True, transform=transform_pipeline, download=True)
 test_dataset = FashionMNIST(root=dataset_path, train=False, transform=transform_pipeline, download=True)
+
 # ----- Check the range of a random tensor from the training_dataset
 print(f"Mean of the tensors in the Training is: {(train_dataset.data.float()).mean().item()}")
+
 # ----- Divide the dataset into Training and Validation set
 train_size = len(train_dataset)
 train_length, val_length = int(0.8 * train_size), int(0.2 * train_size) # Divide data by 80-20 rule
 train_set, validation_set = torch.utils.data.random_split(train_dataset, [train_length, val_length])
+
 # ----- Create Dataloader, to iterate through the data batches
 train_loader = data.DataLoader(train_set, batch_size=1024, shuffle=True)
 validation_loader = data.DataLoader(validation_set, batch_size=1024, shuffle=True)
 test_loader = data.DataLoader(test_dataset, batch_size=1024, shuffle=True)
+
 # ---------------------------------------------------Neural Network Architecture-------------------------------------- #
 class BaseNetwork(nn.Module):
     def __init__(self, activation_func, input_size=784, output_classes=10, hidden_layers=[512, 256, 256, 128]):
